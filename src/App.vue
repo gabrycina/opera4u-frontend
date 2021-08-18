f5af320365a8f9fe8a4c0092515158813f5b72b5
gabrycina <c.gabriele.info@gmail.com>
2021-08-18 20:06:21 +0200

    wave-ui framework addition

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <w-app>
       <div id="nav">
         <router-link to="/">Home </router-link> 
         <router-link to="/artists">Artists </router-link> 
@@ -7,7 +7,7 @@
         <router-link to="/about">About</router-link>
       </div>
       <router-view/>
-  </div>
+  </w-app>
 </template>
 
 <script>
